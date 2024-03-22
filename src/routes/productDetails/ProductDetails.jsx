9d895afd0ceded9db5249e62c10dddfbf880622a
@chandan195 <chandanmariya98@gmail.com>
2024-03-22 17:07:03 +0530

    changes button style

--- src/routes/productDetails/ProductDetails.jsx
+++ src/routes/productDetails/ProductDetails.jsx
@@ -50,17 +50,17 @@ const ProductDetails = () => {
           if (product.id == param.id) {
             return (
               <div
-                className="pt-28 bg-black/40 lg:bg-center lg:pt-64  pb-24 w-full h-full bg-center  flex "
+                className="pt-28 bg-black/40 lg:bg-center lg:pt-64  pb-24 w-full h-full bg-center  flex  "
                 key={product.id}
               >
                 <button className="lg:pr-72">.....</button>
                 {/* <div key={product.id} className=" bg-slate-400 rounded-lg"> */}
-                <div className="h-52 w-96">
+                <div className="h-52 w-96 ">
                   <img
                     loading="lazy"
                     src={product.ItemImg}
                     alt={product.ItemName}
-                    className="h-96 w-96 object-cover object-center lg:h-full lg:w-full"
+                    className="h-96 w-96 object-cover rounded-lg object-center lg:h-full lg:w-full"
                   />
                 </div>
                 <div>
@@ -70,7 +70,7 @@ const ProductDetails = () => {
                         {product.ItemName}
                       </h3>
 
-                      <p className="font-medium text-white">
+                      <p className="font-medium text-white w-2/3">
                         {product.ItemIngredients}
                       </p>
                     </div>
@@ -83,10 +83,11 @@ const ProductDetails = () => {
                       </p>
                     </div>
                     <div
-                      className=" text-md font-bold  text-white text-center 
-                     rounded-lg w-3/5  bg-green-500  p-3 m-3 hover:bg-green-500/75"
+                      className="   text-white 
+                     rounded-lg  bg-green-500   hover:bg-green-500/75"
                     >
-                      <button className="" onClick={() => handleAdd(product)}>
+                      <button className="    text-white 
+                     rounded-lg  text-md font-bold text-center w-full h-1/3 p-3 bg-center" onClick={() => handleAdd(product)}>
                         Add To Card
                       </button>
                     </div>
