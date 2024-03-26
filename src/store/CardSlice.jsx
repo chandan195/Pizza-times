2fa108ad733ea0403b8b19333a975ec18baf8832
@chandan195 <chandanmariya98@gmail.com>
2024-03-26 15:39:38 +0530

    add cart item in locat storage

--- src/store/CardSlice.jsx
+++ src/store/CardSlice.jsx
@@ -1,9 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const cardSlices = createSlice({
   name: "card",
   initialState: {
-    cart: [],
+    cart: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
   },
   reducers: {
     addToCart: (state, action) => {
@@ -17,27 +20,48 @@ const cardSlices = createSlice({
       } else {
         state.cart.push(action.payload);
       }
+     
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
     },
     removeFromCart: (state, action) => {
       state.cart = state.cart.filter((item) => item.id !== action.payload);
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
     },
+
+    clearCard: (state, action) => {
+      state.cart = [];
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
+      toast.error("Cart cleared", { position: "top-center" });
+    },
+
     incrementQty: (state, action) => {
-      // //* ToDo: handle increment
-      // console.log( "id slice",action.payload)
       state.cart = state.cart.map((item) =>
         item.id === action.payload ? { ...item, qty: item.qty + 1 } : item
       );
-     
+      toast.success("Product qty increase from cart" ,{position:"top-center"})
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
     },
+
     decrementQty: (state, action) => {
       //* ToDo: handle increment
       state.cart = state.cart.map((item) =>
         item.id === action.payload ? { ...item, qty: item.qty - 1 } : item
       );
+
+      toast.error("Product qty removed from cart", {
+        position: "top-left",
+      });
+      localStorage.setItem("cartItems", JSON.stringify(state.cart));
     },
   },
 });
 
-export const { addToCart, removeFromCart, incrementQty, decrementQty } =
-  cardSlices.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQty,
+  decrementQty,
+  clearCard,
+} = cardSlices.actions;
 export default cardSlices.reducer;
